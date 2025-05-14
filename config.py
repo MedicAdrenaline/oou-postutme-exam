2087cbea91b691078243f9b3ae0fb1969c22c71e
Medic Adrenaline <medicadrenaline@gmail.com>
2025-05-14 08:59:13 -0700

    Updated config.py to include DB_PORT

--- config.py
+++ config.py
@@ -11,6 +11,7 @@ class Config:
     DB_USER = os.getenv('DB_USER')
     DB_PASSWORD = os.getenv('DB_PASSWORD')
     DB_NAME = os.getenv('DB_NAME')
+    DB_PORT = os.getenv('DB_PORT')
 
     # Paystack and WhatsApp
     PAYSTACK_PUBLIC_KEY = os.getenv('PAYSTACK_PUBLIC_KEY')
