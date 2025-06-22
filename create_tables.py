a21d9f97ee4f33cfd604aa39197efe6460440d4b
Medic Adrenaline <medicadrenaline@gmail.com>
2025-06-22 16:21:43 -0700

    Remove sensitive scripts from tracking

--- create_tables.py
+++
@@ -1,7 +0,0 @@
-from app import app, db, Admin
-
-with app.app_context():
-    # Explicitly reference Admin so it's not skipped
-    _ = Admin
-    db.create_all()
-    print("Tables created successfully.")
